3c152327b866d7db989759a4e5db534f9db3b98c
Belal-Bari <tanvirj9@gmail.com>
2025-12-01 08:40:37 +0200

    Completed

--- spin_words.py
+++ spin_words.py
@@ -1,37 +1,39 @@
-i = "Hey fellow warriors"
+i = input("Give a sentence:\n")
 
 def spin_words(sentence):
-    # Your code goes here
     temp = ''
     reverse = ''
     output = ''
-    #print(temp)
-    for letter in range(len(sentence)):
-        #print(f"Iter {letter}")
-        if letter > 0 and letter < len(i)-1:
-            if i[letter] != ' ':
-                temp += i[letter]
-                #print("temp->", temp)
-            else:
+    for letter in range(len(sentence)):                
+        if i[letter].isalpha() or i[letter].isdigit() :
+            temp += i[letter]
+            if letter == len(sentence)-1:
                 if len(temp) > 4:
                     for j in range(len(temp)-1, -1, -1):
                         reverse += temp[j]
                     temp = reverse
                     reverse = ''
-
-                    output += temp + ' '
+                    output += temp
                     temp = ''
                 else:
-                    output += temp + ' '
+                    output += temp
                     temp = ''
-        elif letter == len(i)-1:
-            temp += i[letter]
-            #print("last", temp)
-            output += temp
         else:
+            if len(temp) > 4:
+                for j in range(len(temp)-1, -1, -1):
+                    reverse += temp[j]
+                output += reverse
+                reverse = ''
+                temp = ''
+
             temp += i[letter]
-            #print("0->",temp)
-    
+            output += temp
+            temp = ''
+
+        if letter == 0 or letter == len(sentence)-1:
+            if i[letter] != '"':
+                output += '"'
+
     print(output)
     return output
 
